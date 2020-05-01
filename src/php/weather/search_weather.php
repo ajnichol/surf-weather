28e362555e63adafcdfb0cf4d15468dd32719b03
Alex Nichols <anichols@meetsoci.com>
2020-04-30 17:28:14 -0700

    Making successful request to open weather

--- src/php/weather/search_weather.php
+++ src/php/weather/search_weather.php
@@ -1,6 +1,12 @@
 <?php
   // require db connection
   require_once '../db_connection.php';
+  // and env vars
+  require '../../../vendor/autoload.php';
+  $dotenv = Dotenv\Dotenv::createImmutable('../../../');
+  $dotenv->load();
+
+  const URL = 'http://api.openweathermap.org/data/2.5/forecast?';
   // get form data
   $form_data = json_decode(file_get_contents('php://input'), true);
 
@@ -9,3 +15,16 @@
     echo('All fields are required.');
     return;
   }
+
+  $params = http_build_query([
+    'q' => $form_data['city'],
+    'units' => 'imperial',
+    'appid' => $_ENV['OWM_KEY']
+  ]);
+
+  $weather_data = json_decode(file_get_contents(URL . $params), true);
+
+  // create data structure to return to frontend that can map to your
+  // weather model
+
+  var_dump($weather_data);
